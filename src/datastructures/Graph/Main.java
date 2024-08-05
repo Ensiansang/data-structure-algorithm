0a0dc2c1a2edc2a7ef7f3635527684637a2cd23b
Ensiansang <dalepipi18@gmail.com>
2024-08-06 05:29:51 +0630

    Graph

---
+++ src/datastructures/Graph/Main.java
@@ -0,0 +1,25 @@
+package datastructures.Graph;
+
+public class Main {
+
+    public static void main(String[] args){
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addVertex("D");
+
+        myGraph.addEdge("A","B");
+        myGraph.addEdge("A","C");
+        myGraph.addEdge("A","D");
+        myGraph.addEdge("B","D");
+        myGraph.addEdge("C","D");
+
+        myGraph.removeVertex("D");
+
+     //   myGraph.removeEdge("A","B");
+        myGraph.printGraph();
+    }
+
+}
